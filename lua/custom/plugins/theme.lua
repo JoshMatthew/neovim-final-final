4ef70bcb0649637e5f8022d695e8ba97eaffaa65
Josh Matthew Talplacido <mateo.talplacido.24@gmail.com>
2026-01-29 03:56:27 +0800

    added and changed theme to catppuccin

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,10 +1,21 @@
+-- return {
+--   'bluz71/vim-moonfly-colors',
+--   name = 'moonfly',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     ---@diagnostic disable-next-line: missing-fields
+--     vim.cmd.colorscheme 'moonfly'
+--   end,
+-- }
+
+-- for light mode
 return {
-  'bluz71/vim-moonfly-colors',
-  name = 'moonfly',
+  'catppuccin/nvim',
+  name = 'catppuccin',
   lazy = false,
   priority = 1000,
   config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    vim.cmd.colorscheme 'moonfly'
+    vim.cmd.colorscheme 'catppuccin-mocha'
   end,
 }
