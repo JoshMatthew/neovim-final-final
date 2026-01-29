91b0b8b4c41ecc29ce6267b9f8b59a6c7ac5c40e
Josh Matthew Talplacido <mateo.talplacido.24@gmail.com>
2026-01-29 08:06:21 +0800

    show hidden files

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -6,6 +6,20 @@ return {
     opts = {},
     -- Optional dependencies
     dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
+    config = function()
+      require('oil').setup {
+        default_file_explorer = true,
+        delete_file = true,
+        skip_confirm_form_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+          is_always_hidden = function(name, _)
+            return name == '..' or name == '.git'
+          end,
+        },
+      }
+    end,
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
