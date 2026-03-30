e7d1b6eb8d6213aafde031efb8486602435797c0
Josh Matthew Talplacido <mateo.talplacido.24@gmail.com>
2026-03-31 00:44:21 +0800

    updates, I don't know what happened anymore. But it's better. Trust me

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -16,7 +16,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'catppuccin-frappe'
-    -- vim.cmd.colorscheme 'catppuccin-mocha'
+    -- vim.cmd.colorscheme 'catppuccin-latte'
+    vim.cmd.colorscheme 'catppuccin-mocha'
   end,
 }
