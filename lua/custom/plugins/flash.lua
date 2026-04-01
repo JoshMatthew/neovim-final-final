f20a613bdddd8d3b6c6a9a3813554155094e867a
Josh Matthew Talplacido <mateo.talplacido.24@gmail.com>
2026-04-01 08:34:56 +0800

    tune flash.nvim: disable multi-window search and prompt for snappier response

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -2,7 +2,14 @@ return {
   {
     'folke/flash.nvim',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+      search = {
+        multi_window = false,
+      },
+      prompt = {
+        enabled = false,
+      },
+    },
     keys = {
       { 's', function() require('flash').jump() end, mode = { 'n', 'x', 'o' }, desc = 'Flash jump' },
       { 'S', function() require('flash').treesitter() end, mode = { 'n', 'x', 'o' }, desc = 'Flash treesitter' },
